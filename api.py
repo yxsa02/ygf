5ee49c43de282ba4cc88283a11eedfefd155fd23
yxs <atwyxsa02@163.com>
2025-12-24 12:28:04 +0800

    create api

---
+++ api.py
@@ -0,0 +1,5 @@
+import requests
+#user-agent
+UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36 Edg/143.0.0.0"
+cookie = "wordpress_logged_in_14420c6ebb1570d14f2f54f3f2ad5843=phone_312728090%7C2076811748%7CLvTz4WTxW4bReILvVeI0ds4AgXQ03GUm9f3UzV9QFPA%7Cabb5c8e3cdd0c92f8762ad6e4dcccdca3f8d79070bea02a91196cfcdc5cc6869"
+
